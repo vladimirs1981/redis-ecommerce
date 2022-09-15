a7378c8e7fcf06e6a38a928bd6b35d46b33adfeb
vladimirs1981 <st.vladas@hotmail.com>
2022-09-15 15:34:38 +0200

    hset users

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,19 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+    await client.hSet('airplane', {
+        color: 'red',
+        year: 1985,
+        
+    });
+
+    const airplane = await client.hGetAll('airplane');
+
+    if(Object.keys(airplane).length === 0) {
+        console.log('Airplane not found. 404.');
+        return;
+    }
+    console.log(airplane);
+};
 run();
--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,8 +1,23 @@
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
+import { client } from '$services/redis';
+import { usersKey } from '$services/keys';
+
 
 export const getUserByUsername = async (username: string) => {};
 
 export const getUserById = async (id: string) => {};
 
-export const createUser = async (attrs: CreateUserAttrs) => {};
+export const createUser = async (attrs: CreateUserAttrs) => {
+    const id = genId();
+    await client.hSet(usersKey(id), serialize(attrs));
+
+    return id;
+};
+
+const serialize =(user: CreateUserAttrs) => {
+    return {
+        username: user.username,
+        password: user.password
+    }
+}
